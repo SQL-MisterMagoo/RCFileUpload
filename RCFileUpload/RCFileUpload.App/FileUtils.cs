4eb48565c2014799df66f2fbd8ef98831e2d2a29
Mister Magoo <mister.magoo+githubsql@gmail.com>
2019-02-28 01:25:50 +0000

    Add project files.

---
+++ RCFileUpload/RCFileUpload.App/FileUtils.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace RCFileUpload.App
+{
+	public static class FileUtils
+	{
+		[JSInvokable]
+		public static Task<bool> LoadFile(string name, string value)
+		{
+			byte[] FileData;
+			string FileName;
+			try
+			{
+				if (value.Contains(";base64,"))
+				{
+					FileData = Convert.FromBase64String(value.Substring(value.IndexOf("base64") + 7));
+				}
+				else
+				{
+					FileData = GetBytes(value);
+				}
+				FileName = $"wwwroot\\uploads\\{System.IO.Path.GetFileName(name)}";
+				System.IO.File.WriteAllBytes(FileName, FileData);
+			}
+			catch (Exception ex)
+			{
+			}
+			return Task.FromResult(true);
+
+			byte[] GetBytes(string str)
+			{
+				byte[] bytes = new byte[str.Length * sizeof(char)];
+				System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+				return bytes;
+			}
+		}
+	}
+}
